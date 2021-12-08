dd7dedea0cca193d84a6d30fc86b6ba567bb5958
Ariell Dattage <ariell.dattage@Ariells-MacBook-Pro.local>
2021-12-08 14:30:15 -0700

    added functionality

--- ratings.py
+++ ratings.py
@@ -1,28 +1,53 @@
 """Restaurant rating lister."""
 
 
-import os
-import sys
+open_file = open('scores.txt')
 
-with open(os.path.join(sys.path[0], "scores.txt"), "r") as f:
-   print(f.read())
 
-restaurant = {
-    "Florean Fortescue's Ice Cream Parlour" : 4,
-    "Jellied Eel Shop" : 3,
-    "The Tavern" : 3,
-    "Luchino Caffe" : 1,
-    "The Porcupine" : 5,
-    "Diagon Alley cafe" : 2,
-    "The Bear & Staff" : 2,
-    "Ministry Munchies" : 1,
-    "Chip Shop" : 3,
-    "Eternelle's Elixir of Refreshment" : 5,
-    "Big Bean Shack" : 3,
-    "The Club" : 2
-}
+def score_compiler(file):
+    dict = []
+    for i in file:
+        values = i.split(':')
+        name = values[0]
+        rating = values[1].strip()
+        dict.append({'name': name,
+                     'rating': rating})
+    return dict
 
-name = restaurant.keys
-rating = restaurant.values
 
-print (f"{name} is rated at {rating}.")
+score_dict = score_compiler(open_file)
+open_file.close()
+
+
+def sorted_loop(dict):
+    loop_listed = []
+    for i in dict:
+        name = i.get('name')
+        rating = i.get('rating')
+        loop_listed.append(f"{name} is rated at {rating}.")
+    return sorted(loop_listed)
+
+
+def user_access(dict):
+    while True:
+        user_input = input("What would you like to do?\n")
+        if (user_input == 'add'):
+            name = input("What's the name of the resturant?\n")
+            rating = int(input("How would you rate it?\n"))
+            dict.append({'name': name,
+                        'rating': rating})
+            looper = sorted_loop(dict)
+            for i in looper:
+                print(i)
+        elif(user_input == 'view'):
+            looper = sorted_loop(dict)
+            for i in looper:
+                print(i)
+        elif(user_input == 'exit'):
+            print("Ok, bye!\n")
+            break
+        else:
+            print("Invalid input. Please try again.\nType one of the following options: 'view', 'add', or 'exit'\n")
+
+
+user_access(score_dict)
